443346cddb6791c04261947315eac3684723b9a6
oprotsenko <misterrygirl@gmail.com>
2018-03-03 22:17:39 +0200

    lesson 6

--- SpiralCreatorNEW.java
+++ SpiralCreatorNEW.java
@@ -7,36 +7,39 @@ public class SpiralCreatorNEW {
         int rows = 7;
         int cols = 9;
 
-        printSpiral(createSpiral(rows,cols));
+        printSpiral(createSpiral(rows, cols));
     }
 
     private static int[][] createSpiral(int rows, int cols) {
         checkForNegative(rows, cols);
         int[][] spiralMatrix = new int[rows][cols];
         int r = 0, c = 1, filler = 1, x = 0, y = 0;
-
         for (; filler <= rows * cols; r++, c++) {
             if (x == r && y == r)
                 for (; x <= cols - c; x++) spiralMatrix[y][x] = filler++;
             if (x == cols - r) {
                 x--;
                 y++;
-            } if (x == cols - c && y == c)
+            }
+            if (x == cols - c && y == c)
                 for (; y <= rows - c; y++) spiralMatrix[y][x] = filler++;
             if (y == rows - r) {
                 y--;
                 x--;
-            } if (y == rows - c && x == cols - c - 1)
+            }
+            if (y == rows - c && x == cols - c - 1)
                 for (; x >= r; x--) spiralMatrix[y][x] = filler++;
             if (x == r - 1) {
                 x++;
                 y--;
-            } if (x == r && y == rows - c - 1)
+            }
+            if (x == r && y == rows - c - 1)
                 for (; y >= c; y--) spiralMatrix[y][x] = filler++;
             if (y == r) {
                 y++;
                 x++;
-            } if (x == c && y == c)
+            }
+            if (x == c && y == c)
                 for (; x <= cols - (c + 1); x++) spiralMatrix[y][x] = filler++;
         }
         return spiralMatrix;
